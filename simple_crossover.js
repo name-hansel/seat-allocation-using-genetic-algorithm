87bb50caa686c2f1fa88aa82673f6292dc1a7ccd
Snehal Mane <snehal6mane@gmail.com>
2022-03-06 14:58:26 +0530

    Skip both i,j on clash works in alternating crossover

--- simple_crossover.js
+++ simple_crossover.js
@@ -20,11 +20,11 @@ function isSeatEmpty(seat) {
 }
 
 const parentOne = [[1], [2], [3], [4], [5], [6], [7], [8], [], [], []]
-const parentTwo = shuffleArray(parentOne)
+const parentTwo = shuffleArray(parentOne);
 
 const geneLength = parentOne.length
 const emptySeats = 3;
-const offspring = parentOne.map(() => [])
+const offspring = parentOne.map(() => [null])
 
 // Create object which contains seats already present in offspring
 const elementsAlreadyPresentInOffspring = {};
@@ -36,27 +36,82 @@ var numberOfEmptySeats = 0;
 const parents = [parentOne, parentTwo];
 var currentParent = 0;
 
-// Gene position in parent
-var j = 0;
-var i = 0;
-
-for (; i < geneLength; j = (j + 1) % geneLength) {
+for (let i = 0; i < geneLength; i++, currentParent = (currentParent + 1) % 2) {
   parent = parents[currentParent];
-  if (isSeatEmpty(parent[j])) {
+  if (isSeatEmpty(parent[i])) {
     if (numberOfEmptySeats < emptySeats) {
-      offspring[i] = parent[j];
+      offspring[i] = parent[i];
       numberOfEmptySeats++;
-      i++;
     }
-
   } else {
-    if (elementsAlreadyPresentInOffspring[parent[j]] !== 1) {
-      offspring[i] = parent[j];
-      elementsAlreadyPresentInOffspring[parent[j]] = 1;
-      i++;
+    if (elementsAlreadyPresentInOffspring[parent[i][0]] !== 1) {
+      offspring[i][0] = parent[i][0];
+      elementsAlreadyPresentInOffspring[parent[i][0]] = 1;
     }
   }
-  currentParent = (currentParent + 1) % 2;
 }
 
-console.log(offspring)
+var i = 0;
+var j = 0;
+while (i < geneLength) {
+  if (isSeatEmpty(offspring[i])) {
+    i++;
+    continue;
+  }
+  if (offspring[i][0] !== null) {
+    i++;
+    continue;
+  }
+
+  if (isSeatEmpty(parentTwo[j])) {
+    if (numberOfEmptySeats < emptySeats) {
+      offspring[i] = parentTwo[j];
+      i++;
+      j++;
+      continue;
+    } else {
+      j++;
+      continue;
+    }
+  } else {
+    if (elementsAlreadyPresentInOffspring[parentTwo[j][0]] === 1) {
+      j++;
+      continue;
+    } else {
+      offspring[i] = parentTwo[j];
+      elementsAlreadyPresentInOffspring[parentTwo[j][0]] = 1;
+      i++;
+      j++;
+      continue;
+    }
+  }
+}
+
+console.log(parentOne);
+console.log(parentTwo);
+console.log(offspring);
+
+
+
+// Gene position in parent
+// var j = 0;
+// var i = 0;
+
+// for (; i < geneLength; j = (j + 1) % geneLength) {
+//   parent = parents[currentParent];
+//   if (isSeatEmpty(parent[j])) {
+//     if (numberOfEmptySeats < emptySeats) {
+//       offspring[i] = parent[j];
+//       numberOfEmptySeats++;
+//       i++;
+//     }
+
+//   } else {
+//     if (elementsAlreadyPresentInOffspring[parent[j]] !== 1) {
+//       offspring[i] = parent[j];
+//       elementsAlreadyPresentInOffspring[parent[j]] = 1;
+//       i++;
+//     }
+//   }
+//   currentParent = (currentParent + 1) % 2;
+// }
