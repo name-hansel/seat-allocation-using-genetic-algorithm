07b079e0a53dbf371f922b850ae3347172cc11ca
Snehal Mane <snehal6mane@gmail.com>
2022-03-27 12:08:27 +0530

    PMX crossover

--- simple_crossover.js
+++ simple_crossover.js
@@ -19,99 +19,98 @@ function isSeatEmpty(seat) {
   return seat.length === 0;
 }
 
-const parentOne = [[1], [2], [3], [4], [5], [6], [7], [8], [], [], []]
-const parentTwo = shuffleArray(parentOne);
-
+const parentOne = [[1], [2], [3], [4], [5], [6], [7], [8], [9], []]
 const geneLength = parentOne.length
-const emptySeats = 3;
-const offspring = parentOne.map(() => [null])
 
-// Create object which contains seats already present in offspring
-const elementsAlreadyPresentInOffspring = {};
-
-// Keep track of number of empty seats in offspring
-var numberOfEmptySeats = 0;
-
-// Keep track of current parent
-const parents = [parentOne, parentTwo];
-var currentParent = 0;
-
-for (let i = 0; i < geneLength; i++, currentParent = (currentParent + 1) % 2) {
-  parent = parents[currentParent];
-  if (isSeatEmpty(parent[i])) {
-    if (numberOfEmptySeats < emptySeats) {
-      offspring[i] = parent[i];
-      numberOfEmptySeats++;
-    }
-  } else {
-    if (elementsAlreadyPresentInOffspring[parent[i][0]] !== 1) {
-      offspring[i][0] = parent[i][0];
-      elementsAlreadyPresentInOffspring[parent[i][0]] = 1;
-    }
+// If any empty seats, fill them 'EMPTY_NUMBER'
+for (let i = 0, j = 1; i < geneLength; i++) {
+  if (parentOne[i].length === 0) {
+    parentOne[i][0] = `EMPTY_${j}`;
+    j++;
   }
 }
 
-var i = 0;
-var j = 0;
+const parentTwo = shuffleArray(parentOne);
+// const parentTwo = [[9], [3], [7], [8], [2], [6], [5], [1], [4]]
+
+const offspring = parentOne.map(() => [])
+
+// Generate breakpoints for random segment
+const [x, y] = generateBreakpoints(geneLength);
+
+const elementsInOffspring = {};
+const elementMappingPTwo = {};
+
+// Copy elements from P1 to offspring in x to y
+for (let i = x; i <= y; i++) {
+  offspring[i][0] = parentOne[i][0];
+  elementsInOffspring[offspring[i][0]] = i;
+}
+
+// Get element and their position in parentTwo
+for (let i = 0; i < geneLength; i++)
+  elementMappingPTwo[parentTwo[i][0]] = i;
+
+for (let i = x; i <= y; i++) {
+  if (elementsInOffspring[parentTwo[i][0]]) {
+    continue;
+  }
+
+  let j = i;
+  while (j >= x && j <= y) {
+    j = elementMappingPTwo[parentOne[j][0]];
+  }
+  offspring[j][0] = parentTwo[i][0];
+  elementsInOffspring[offspring[j][0]] = j;
+}
+
+// Fill in the remaining genes from parentTwo
+// Part one, before breakpoint x
+// i = index in offspring
+// j = index in parentTwo
+let i = 0, j = 0;
+while (i < x) {
+  // Check if empty
+  if (offspring[i].length !== 0) {
+    i++;
+    continue;
+  }
+  if (elementsInOffspring[parentTwo[j][0]]) {
+    // Already present in offspring
+    j++;
+  } else {
+    // Not present in offspring
+    offspring[i][0] = parentTwo[j][0];
+    elementsInOffspring[offspring[i][0]] = i;
+    i++;
+    j++;
+  }
+}
+
+// Part two, after breakpoint x
+// i = index in offspring
+// j = index in parentTwo
+i = y + 1;
 while (i < geneLength) {
-  if (isSeatEmpty(offspring[i])) {
+  // Check if empty
+  if (offspring[i].length !== 0) {
     i++;
     continue;
   }
-  if (offspring[i][0] !== null) {
-    i++;
-    continue;
-  }
-
-  if (isSeatEmpty(parentTwo[j])) {
-    if (numberOfEmptySeats < emptySeats) {
-      offspring[i] = parentTwo[j];
-      i++;
-      j++;
-      continue;
-    } else {
-      j++;
-      continue;
-    }
+  if (elementsInOffspring[parentTwo[j][0]]) {
+    // Already present in offspring
+    j++;
   } else {
-    if (elementsAlreadyPresentInOffspring[parentTwo[j][0]] === 1) {
-      j++;
-      continue;
-    } else {
-      offspring[i] = parentTwo[j];
-      elementsAlreadyPresentInOffspring[parentTwo[j][0]] = 1;
-      i++;
-      j++;
-      continue;
-    }
+    // Not present in offspring
+    offspring[i][0] = parentTwo[j][0];
+    elementsInOffspring[offspring[i][0]] = i;
+    i++;
+    j++;
   }
 }
 
-console.log(parentOne);
-console.log(parentTwo);
-console.log(offspring);
-
-
-
-// Gene position in parent
-// var j = 0;
-// var i = 0;
-
-// for (; i < geneLength; j = (j + 1) % geneLength) {
-//   parent = parents[currentParent];
-//   if (isSeatEmpty(parent[j])) {
-//     if (numberOfEmptySeats < emptySeats) {
-//       offspring[i] = parent[j];
-//       numberOfEmptySeats++;
-//       i++;
-//     }
-
-//   } else {
-//     if (elementsAlreadyPresentInOffspring[parent[j]] !== 1) {
-//       offspring[i] = parent[j];
-//       elementsAlreadyPresentInOffspring[parent[j]] = 1;
-//       i++;
-//     }
-//   }
-//   currentParent = (currentParent + 1) % 2;
-// }
+console.log(parentOne)
+console.log(parentTwo)
+console.log(x, y)
+console.log(offspring)
+// console.log(elementsInOffspring);
